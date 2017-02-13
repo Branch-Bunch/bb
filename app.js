4b2ce2497556765fbb13fe9a713192958e336a94
bkzhang <brian.k.zhang@gmail.com>
2017-02-13 00:53:52 -0500

    not sure what last comit was but ok

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const { app, BrowserWindow } = require('electron')
+const { app } = require('electron')
 const appExpress = require('express')()
 const path = require('path')
 const url = require('url')
@@ -22,45 +22,7 @@ class BB {
     app.on('ready', () => {
       this.createQueryWindow()
       this.queryWindow.loadURL('http://localhost:3000')
-      /*
-      this.queryWindow.loadURL(url.format({
-        pathname: path.join(__dirname, '/windows/views/main/index.html'),
-        protocol: 'file',
-        slashes: true
-      })) 
-      */
       this.queryWindow.devTools()
-
-      /*
-      const {net} = require('electron')
-      const request = net.request('http://127.0.0.1:1337')
-      request.on('response', (response) => {
-        console.log(`STATUS: ${response.statusCode}`)
-        console.log(`HEADERS: ${JSON.stringify(response.headers)}`)
-        response.on('data', (chunk) => {
-          console.log(`BODY: ${chunk}`)
-        })
-        response.on('end', () => {
-          console.log('no more data in response.')
-        })
-      })
-      request.end() 
-
-      const requestQuery = net.request({
-        method: 'POST',
-        protocol: 'http:',
-        hostname: '127.0.0.1',
-        port: '1337',
-        path: '/queries'
-      })
-      requestQuery.on('response', (response) => {
-        console.log('worked')
-        responseQuery.on('data', (chunk) => {
-          console.log(`BODY: ${chunk}`)
-        })
-        responseQuery.writeHead(301, { "Location": "http://127.0.0.1:1337" })
-      })
-      requestQuery.end() */
     })
 
     app.on('activate', () => {
--- server.js
+++ server.js
@@ -2,16 +2,11 @@
 
 const express = require('express')
 const app = express()
-const fs = require('fs')
 const server = require('http').createServer(app)
 const path = require('path')
+const config = require('./config')
 
 const houndify = require('houndify').HoundifyExpress
-
-const argv = require('minimist')(process.argv.slice(2))
-
-const configFile = argv.config || 'config'
-const config = require(path.join(__dirname, configFile))
 
 const port = config.port || 3030
 
